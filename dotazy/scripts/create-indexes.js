1e3c1b99bcf40d117b203c8f62d0f7a60f732a93
Lukáš Kovář <lkov@post.cz>
2025-05-18 10:32:25 +0200

    reorganize makefile order by command order, fix create-indexes

--- dotazy/scripts/create-indexes.js
+++ dotazy/scripts/create-indexes.js
@@ -11,5 +11,5 @@ db.products.createIndex({ product_category_name: 1 });
 
 // Order Items collection
 db.order_items.createIndex({ order_item_id: 1 });
-db.order_items.createIndex({ order_id: 1 }, { unique: true });
 db.order_items.createIndex({ product_id: 1 });
+db.order_items.createIndex({ order_id: 1, order_item_id: 1 }, { unique: true });
